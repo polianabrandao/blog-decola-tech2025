f6f5402fb3ff3861ffb912b7ff90df846409dafb
Poliana <polibarross@gmail.com>
2025-04-11 15:56:13 -0300

    alteraçoes de layout e logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
+import { MenuNavComponent } from './components/menu-nav/menu-nav.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ContentComponent } from './pages/content/content.component';
     BigCardComponent,
     SmallCardComponent,
     HomeComponent,
-    ContentComponent
+    ContentComponent,
+    MenuNavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/menu-nav/menu-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-nav',
+  standalone: false,
+  templateUrl: './menu-nav.component.html',
+  styleUrl: './menu-nav.component.css'
+})
+export class MenuNavComponent {
+
+}
